6fd31de49a2d399bd5b16d100a8f7a3951559552
HSMathebula <56921581+HSMathebula@users.noreply.github.com>
2022-05-25 19:56:59 +0200

    Fix errors in displayUrl.js

--- src/displayUrl.js
+++ src/displayUrl.js
@@ -1,48 +1,49 @@
 import commentsUrl from './commentsUrl.js';
 
 export default class Movies {
-    static countComment = (id) => {
-        commentsUrl.getComments(id).then((data) => {
-          const display = document.querySelector('.display-comments');
-          const count = document.querySelector('.comment-count');
-          count.textContent = `Comments (${commentsUrl.counterComments(data)})`;
-          display.innerHTML = '';
-          data.forEach((item) => {
-            const commentList = document.createElement('li');
-    
-            commentList.textContent = `${item.creation_date} ${item.username} : ${item.comment}`;
-            display.appendChild(commentList);
+    static displayComment = (id) => {
+      const username = document.querySelector('.user-name');
+      const comment = document.querySelector('.comment');
+      const buttonAdd = document.querySelector('.add-comment');
+      buttonAdd.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        commentsUrl.setComments(id, username.value, comment.value)
+          .then((data) => {
+            if (data === 'Created') {
+              this.countComment(id);
+              username.value = '';
+              comment.value = '';
+            }
           });
-        });
+      });
     };
 
-    static displayComment = (id) => {
-        const username = document.querySelector('.user-name');
-        const comment = document.querySelector('.comment');
-        const buttonAdd = document.querySelector('.add-comment');
-        buttonAdd.addEventListener('click', (e) => {
-          e.preventDefault();
-    
-          commentsUrl.setComments(id, username.value, comment.value)
-            .then((data) => {
-              if (data === 'Created') {
-                this.countComment(id);
-                username.value = '';
-                comment.value = '';
-              }
-            });
+    static countComment = (id) => {
+      commentsUrl.getComments(id).then((data) => {
+        const display = document.querySelector('.display-comments');
+        const count = document.querySelector('.comment-count');
+        count.textContent = `Comments (${commentsUrl.counterComments(data)})`;
+        display.innerHTML = '';
+        data.forEach((item) => {
+          const commentList = document.createElement('li');
+
+          commentList.textContent = `${item.creation_date} ${item.username} : ${item.comment}`;
+          display.appendChild(commentList);
         });
-      };
+      });
+    };
 
     static displayMovies = async () => {
-        const commentBtn = document.querySelectorAll('.button');
+      const response = await fetch(this.url);
+      const data = await response.json();
+      const commentBtn = document.querySelectorAll('.button');
 
-        commentBtn.forEach((button) => {
-            button.addEventListener('click', (event) => {
-                const id = event.target.getAttribute('id');
-                const popUp = data.filter((item) => item.show.id === parseInt(id, 10),)[0].show;
-                const popupDisplay = 
-                `<div class="popup-display">
+      commentBtn.forEach((button) => {
+        button.addEventListener('click', (event) => {
+          const id = event.target.getAttribute('id');
+          const popUp = data.filter((item) => item.show.id === parseInt(id, 10))[0].show;
+          const popupDisplay = `<div class="popup-display">
                     <div class="popup">
                         <div>
                             <div class="fa-times">
@@ -87,17 +88,17 @@ export default class Movies {
                         </div>
                     </div>
                 </div>`;
-                document.body.insertAdjacentHTML('beforeend', popupDisplay);
+          document.body.insertAdjacentHTML('beforeend', popupDisplay);
 
-                const timesBtn = document.querySelectorAll('.fa-times');
-                timesBtn.forEach((item) => {
-                    item.addEventListener('click', () => {
-                        document.querySelector('.popup-display').remove();
-                    });
-                });
-                this.countComment(button.id);
-                this.displayComment(button.id);
+          const timesBtn = document.querySelectorAll('.fa-times');
+          timesBtn.forEach((item) => {
+            item.addEventListener('click', () => {
+              document.querySelector('.popup-display').remove();
             });
+          });
+          this.countComment(button.id);
+          this.displayComment(button.id);
         });
+      });
     }
 }
