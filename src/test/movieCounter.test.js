496496c5a1b4505e23b2d08b7d2934165a4350e6
Pedro Marques <ppmarq1@gmail.com>
2022-05-26 13:32:07 +0100

    Final tests before deployment

--- src/test/movieCounter.test.js
+++ src/test/movieCounter.test.js
@@ -2,5 +2,5 @@ import Movies from '../displayUrl.js';
 
 test('Display number of movies', async () => {
   const counter = await Movies.counterMovies();
-  expect(counter).toBe(9);
+  expect(counter).toBe(10);
 });
